ca3495a599d5d574d12fd6915139ab4910f457e3
Recendezz <recendezzanetta@gmail.com>
2026-04-17 11:05:19 -0700

    updated html and css

---
+++ js/custom-inquiry.js
@@ -0,0 +1,16 @@
+
+const form = document.querySelector("cannonsCoffeeForm");
+console.log(form)
+const message = document.querySelector("#message");
+console.log(message);
+
+
+// form.addEventListener("submit,function (event)") {
+//     event.preventDefault();
+    
+    const name = document.querySelector("#name");
+    const email = document.querySelector("#email");
+    const phone = document.querySelector("#phone");
+    const comments = document.querySelector("#comments");
+    const flavor = document.querySelector("#flavor");
+    
