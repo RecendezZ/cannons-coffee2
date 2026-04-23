16315a857b08816e742d694b2adf151f6b53178d
Recendezz <recendezzanetta@gmail.com>
2026-04-23 11:13:13 -0700

    updated java script

---
+++ js/custom-login.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("#cannonsCoffeeForm");
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const dataObject = Object.fromEntries(formData.entries());
+
+  console.log(dataObject);
+  form.reset();
+}
+
+form.addEventListener("submit", onFormSubmit);
